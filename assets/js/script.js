cbbb68474f7c50b96e2f71e1012cec7947c070a1
nomeski80 <naomi_hancock@yahoo.co.uk>
2024-03-21 20:31:23 +0000

    added play button and results area. Started to add some js code to create variables to get information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log ("conneted");
+let compChoice = document.getElementById('computer-choice')
+let yourChoice = document.getElementById('user-choice')
+let resultDisplay = document.getElementById('result')
+let playButton = document.getElementById('play')
+let possibleChoices = ['rock', 'paper', 'scissors']
