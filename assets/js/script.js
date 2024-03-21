99d5a8be7243eb0970ed160f6b64b9cdff7ac8b6
nomeski80 <naomi_hancock@yahoo.co.uk>
2024-03-21 21:31:00 +0000

    added some js code to get elements from the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,20 @@ let yourChoice = document.getElementById('user-choice')
 let resultDisplay = document.getElementById('result')
 let playButton = document.getElementById('play')
 let possibleChoices = ['rock', 'paper', 'scissors']
+
+let choices = document.getElementsByName('choice')
+choices.forEach((c) => {
+    c.addEventListener('click', () => {
+        playButton.disabled = false
+    })
+})
+
+playButton.addEventListener('click', (e) => {
+    let buttonsArray = Array.from(choices)
+    let selected = buttonsArray.filter((b) => b.checked)
+    addTextToSpan(yourChoice, selected[0].id)
+})
+
+function addTextToSpan(spanControl, text) {
+    spanControl.textContent = text
+}
