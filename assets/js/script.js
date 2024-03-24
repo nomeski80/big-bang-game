5a90e8976794f13652a1ae5f9f07099d4f33479f
nomeski80 <naomi_hancock@yahoo.co.uk>
2024-03-24 13:41:16 +0000

    fixed a spelling mistake in my HTML which was stopping the computer display. added in the other elements for the if statments. Results ar enow functional too.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,58 @@
 // Wait for the DOM to finish loading befre running the game
 //Document.addEventListener("DOMContentLoaded", function () {
- //   let buttons = document.getElementsByTagName("button")
+//   let buttons = document.getElementsByTagName("button")
 
-   // function runGame() {
+// function runGame() {
 
-  //  }
-    
-// Get the button elements and add event listeners to them
-// explain the rules and how to play
-//encourage player to select their choice
-//function displayPlayerChoice() {
-
-//}
-//computer picks a choice
-
-console.log("ready")
+//  }
 
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
-const resultDisplay = document.getElementById ("resultDisplay")
+const resultDisplay = document.getElementById("resultDisplay")
 
 function playGame(playerChoice) {
 
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-let result = "";
+    let result = "";
 
-if(playerChoice === computerChoice){
-    result = "It's a tie!"
-} else {
-    switch(playerChoice){
-        case "rock":
-            result = (computerChoice === "scissors") ? "YOU WIN!" : " YOU LOSE";
-            break;
-        case "paper":
-                result = (computerChoice === "rock") ? "YOU WIN!" : " YOU LOSE";
+    // compares the choices
+
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!"
+    } else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors", "lizard") ? "YOU WIN!" : " YOU LOSE";
                 break;
-        case "scissors":
-                result = (computerChoice === "paper") ? "YOU WIN!" : " YOU LOSE";
+            case "paper":
+                result = (computerChoice === "rock", "spock") ? "YOU WIN!" : " YOU LOSE";
                 break;
+            case "scissors":
+                result = (computerChoice === "paper", "lizard") ? "YOU WIN!" : " YOU LOSE";
+                break;
+            case "lizard":
+                result = (computerChoice === "paper", "spock") ?"YOU WIN!" : "YOU LOSE";
+                break
+                case "spock":
+                result = (computerChoice === "rock", "scissors") ? "YOU WIN!" : " YOU LOSE";
+                break;
+        }
     }
-}
+    //need to add a call function here to 'if' you win incrementScore
 
-playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-computerDisplay.textContent = `Computer ${computerChoice}`;
-resultDisplay.textContent = result;
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    // display winner
+    resultDisplay.textContent = result;
 
-//function diaplayComputerChoice() {
+    //add score
 
-//}
-// compares the choices
-// display winner
+    function incrementScore() {
+        let oldScore = parseInt(document.getElementById("user-score").innerText);
+        document.getElementById("user-score").innerText + oldScore;
+    }
 
-//add score
 
-//} 
 
 }
